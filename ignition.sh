0269871beb98e3d618280605c946e4242f3c78d6
Timothy Pillow <oregonpillow@gmail.com>
2023-07-29 15:38:12 +0200

    Update ignition.sh
    add rectangle (magnet replacement)

--- ignition.sh
+++ ignition.sh
@@ -6,7 +6,7 @@ set -e
 # ===== VARIABLES =====
 SSH_COMMENT="${USER}@$(hostname)"
 BREW_APPS="tmux m-cli htop btop neofetch wget zsh ansible yt-dlp wireguard-tools mpv"         
-CASK_APPS="anki docker raycast bitwarden spotify sublime-text iterm2 hot monitorcontrol postman joplin transmission mark-text visual-studio-code firefox sabnzbd eloston-chromium"
+CASK_APPS="anki docker rectangle raycast bitwarden spotify sublime-text iterm2 hot monitorcontrol postman joplin transmission mark-text visual-studio-code firefox sabnzbd eloston-chromium"
 # =====================
 i=0
 

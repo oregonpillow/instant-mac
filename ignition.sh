da6a15d6974e2837686468cf58272f3a2063b061
Timothy Pillow <oregonpillow@gmail.com>
2023-09-25 12:26:34 +0200

    Update ignition.sh

--- ignition.sh
+++ ignition.sh
@@ -6,7 +6,7 @@ set -e
 # ===== VARIABLES =====
 SSH_COMMENT="${USER}@$(hostname)"
 BREW_APPS="tmux asciinema m-cli htop btop neofetch wget zsh ansible yt-dlp wireguard-tools mpv"         
-CASK_APPS="anki tabby skype tunnelblick docker rectangle raycast bitwarden spotify sublime-text iterm2 hot monitorcontrol postman joplin transmission-remote-gui mark-text vscodium librewolf sabnzbd eloston-chromium"
+CASK_APPS="anki tabby skype tunnelblick docker rectangle raycast bitwarden spotify sublime-text iterm2 hot monitorcontrol postman joplin transmission-remote-gui mark-text visual-studio-code librewolf sabnzbd eloston-chromium"
 # =====================
 i=0
 

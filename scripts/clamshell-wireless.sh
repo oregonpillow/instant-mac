335ae7830d88a2dd2265bcbd76c5105ea6f5e63e
Timothy Pillow <timothy.pillow@swisscom.com>
2024-05-11 13:21:08 +0200

    update

--- scripts/clamshell-wireless.sh
+++ scripts/clamshell-wireless.sh
@@ -2,11 +2,17 @@
 
 # dependencies: brew install blueutil
 
+# https://apple.stackexchange.com/questions/219885/use-caffeinate-to-prevent-sleep-on-lid-close-on-battery
+
 # collect status
 CLAMCLOSED=$(/usr/sbin/ioreg -r -k AppleClamshellState -d 4 | grep -q '"AppleClamshellState" = Yes' && echo "true" || echo "false")
 BLUEOFF=$(/usr/sbin/system_profiler SPBluetoothDataType | grep -q "State: Off" && echo "true" || echo "false")
 WIFIOFF=$(/usr/sbin/networksetup -getairportpower en0 | grep -q "Wi-Fi Power (en0): Off" && echo "true" || echo "false")
 MUTED=$(/usr/bin/osascript -e "output muted of (get volume settings)")
+
+# disable sleep, otherwise script will not run when state is: battery + clamshell
+sudo /usr/bin/pmset -b disablesleep 1
+
     
 if [ $CLAMCLOSED = "true" ]; then
 
@@ -24,5 +30,8 @@ if [ $CLAMCLOSED = "true" ]; then
   if [ $MUTED = "false" ]; then
     /usr/bin/osascript -e "set volume with output muted"
   fi
+
+  # enable sleep
+  sudo /usr/bin/pmset -b disablesleep 0
 
 fi
---
+++ scripts/set-iphone-tether-dns.sh
@@ -0,0 +1,1 @@
+sudo networksetup -setdnsservers "iPhone USB" 1.1.1.1 10.13.13.1

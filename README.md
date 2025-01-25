2fb12231e6dcc897664bd48e18bc6faa65c83564
debian <oregonpillow@gmail.com>
2025-01-25 01:06:15 +0100

    squash

---
+++ README.md
@@ -0,0 +1,18 @@
+# Instant Mac
+
+💻📡🔨🚀🔥
+
+Spending hours to automate 2 mins. If you can relate, you might like this.
+Simply a script that I can execute on any new mac system to get me up and running.
+
+- 1. Installs Brew
+- 2. Uses Brew to instal ansible
+- 3. Uses ansible for rest...
+
+
+```bash
+bash start.sh
+```
+
+**Tested on ✅**
+- Sonoma 14.7.2
---
+++ start.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Check xcode installed
+xcode-select --print-path &>/dev/null || { echo "⚠️  Script requires xcode tools to be installed. Run 'xcode-select --install'" && exit 1; }
+
+#Check user is not root
+if [ "$EUID" -eq 0 ]; then
+  echo "⚠️  Please don't run directly as root. Use 'bash start.sh'"
+  exit 1
+fi
+
+echo "👌  Preflight checks complete"
+START_TIME="$(date)"
+
+# Prompt for password - needed for some ansible tasks and to install brew
+read -sp "Enter your sudo password: " MY_PASSWORD
+export MY_PASSWORD
+# this is needed to init a sudo session
+echo $MY_PASSWORD | sudo -S echo "🔑  sudo session started"
+
+# Install Brew
+{ test -f /opt/homebrew/bin/brew && echo "✅  Brew already Installed"; } \
+  || { echo -e "\n💻  Installing Brew" \
+    && NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" \
+    && echo "✅  Brew Installed Successfully"; }
+
+# Add Brew to PATH
+test -f ~/.zprofile || touch ~/.zprofile
+{ grep --silent 'eval "$(/opt/homebrew/bin/brew shellenv)"' ~/.zprofile \
+  || echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile; }
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install Ansible
+which ansible > /dev/null 2>&1 && echo "✅  Ansible already Installed" \
+  || { echo -e "\n💻  Installing Ansible" \
+    && brew analytics off \
+    && brew install --no-quarantine --quiet ansible \
+    && echo "✅  Ansible Installed Successfully"; }
+
+# Use ansible for the rest
+cd ansible
+#ansible-galaxy install -r requirements.yml
+ansible-playbook -e "ansible_become_password=$MY_PASSWORD" -i inventory.yml main.yml
+
+# execution time
+END_TIME="$(date)"
+echo "🕒  Script started at: $START_TIME"
+echo "🕒  Script ended at: $END_TIME"

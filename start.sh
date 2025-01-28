8d1fc1e872e9b985fb644a813c846ef91dbaff61
tim <tim@tims-Virtual-Machine.local>
2025-01-28 23:05:50 +0100

    update iterm

--- start.sh
+++ start.sh
@@ -46,40 +46,15 @@ cd ansible
 #ansible-galaxy install -r requirements.yml
 ansible-playbook -e "ansible_become_password=$MY_PASSWORD" main.yml
 
-
-# Install oh-my-zsh (needs zsh, should be installed after brew zsh installed)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
-
 # re-source brew environment
 eval "$(/opt/homebrew/bin/brew shellenv)" && echo "✅Homebrew Environment re-sourced"
 
-# Install dotfiles
-echo "🔧  Installing dotfiles"
-cd $HOME
-yadm clone -f --no-bootstrap https://github.com/oregonpillow/dotfiles.git && \
-# Pause section
-echo "Press Enter to decrypt sensitive config files..."
-read
-yadm decrypt || { echo "⚠️  Failed to decrypt config files. Exiting..." && exit 1; }
 
-# Switch from HTTPS to SSH
-yadm remote set-url origin "git@github.com:oregonpillow/dotfiles.git" && \
-echo "✅ Updated 'oregonpillow/dotfiles.git' repo to SSH to Authentication"
+## restore git to SSH here....
 
-cd instant-mac
-git remote set-url origin "git@github.com:oregonpillow/instant-mac.git" && \
-echo "✅ Updated 'oregonpillow/instant-mac.git' repo to SSH Authentication"
-
-# Set iTerm2 config
-system_type=$(uname -s)
-if [ "$system_type" = "Darwin" ]; then
-  # possibly add something here to ensure iTerm2 is installed using Homebrew
-  # cask like in the previous example
-  if [ -d "$HOME/.iterm2" ]; then
-    echo "Setting iTerm preference folder"
-    defaults write com.googlecode.iterm2 PrefsCustomFolder "$HOME/.iterm2"
-  fi
-fi
+# Write iTerm conf
+defaults write com.googlecode.iterm2 PrefsCustomFolder "$HOME/.iterm2"
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder true
 
 # execution time
 END_TIME="$(date)"
